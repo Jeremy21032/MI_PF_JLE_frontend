ab4d9ad8bb07eb67543291aac5c684b90e23b23c
Jeremy21032 <jeremisma2001@hotmail.com>
2025-07-25 21:48:47 -0500

    Commit made from VS Code Azure Static Web Apps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
-// https://astro.build/config
-export default defineConfig({});
+import node from '@astrojs/node';
+
+export default defineConfig({
+  output: 'static',
+  integrations: [tailwindcss(), react(), icon()],
+
+  env: {
+    schema: {
+      PUBLIC_API_URL: envField.string({ context: 'server', access: 'secret' })
+    }
+  },
+
+  adapter: node({mode: 'standalone'}),
+});
